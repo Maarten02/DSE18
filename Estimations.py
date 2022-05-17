d9ab8da3190ce82d39a16127b0c088b27158ac2a
Tarek Abdelrazek <tarekuni@hotmail.com>
2022-05-17 13:39:27 +0200

    Added Classiter function in Estimations

--- Estimations.py
+++ Estimations.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
-from design_dictionaries import *
 
 
 
@@ -55,6 +54,8 @@ class Aircraft:
         self.f_res= f_res
         self.w_fuel = 0
         self.w_empty = w_empty
+        self.iter = 0
+
 
     def class1(self):
         cruise_fraction = np.exp(self.R/self.efficiency/(9.81*self.c_p)*self.L_D_cruise)
@@ -63,30 +64,58 @@ class Aircraft:
         f2 = 1/loiter_fraction
         fuel_coeff = 1-(f1*f2*self.fractions)
         self.w_mtow = (self.w_paylaod+self.b+self.w_crew)/(1-self.a-fuel_coeff*(1-self.f_res))
+        if self.iter>=1:
+            self.w_oew = self.w_oew
         self.w_oew = self.w_crew +self.w_empty
         self.w_fuel = fuel_coeff* (1+self.f_res)*self.w_mtow
         pass
 
 
     def class2(self):
-        self.class1()
+        # self.class1()
         Ht_Hv = 1
+        ###### Fuselage mass ######
+
         self.m_fuselage = (12.7*(self.length_fus*self.diameter_fus)**(1.2982)*
                           (1-(-0.008*(self.length_fus/self.diameter_fus)**(2)+
                               0.1664*(self.length_fus/self.diameter_fus)-0.8501))*
                           max(self.height_fus,self.width_fus)/self.diameter_fus)
+
+        ###### Main Wing mass ######
+
         self.m_wing = (0.0051*(self.w_design*1.5*2.5)**(0.557)*self.surface_wing**
                       0.649*self.AR**0.5*(self.t_c)**(-0.4)*(1+self.lamda)**0.1*
                       (np.cos(self.sweep_angle))**(-1)*(self.surface_controlv+self.surface_controlh)**0.1)
+
+        ###### Horizontal stabilizer mass ######
+
         self.m_h = 0.016*(1.5*2.5*self.w_design)**0.414*self.q**(0.168)*self.surface_controlh**(0.896)*\
                  (100*self.t_c/np.cos(self.sweep_angle_horizontal))**(-0.12)
+
+        ###### Vertical stabilizer mass######
+
         self.m_v = 0.073*(1+0.2*(Ht_Hv))*(1.5*2.5*self.w_design)**0.376*self.q*0.122*self.surface_controlv**(0.873)*\
                    (100*self.t_c/(np.cos(self.sweep_angle_vertical)))**(-0.49)*\
                    (self.AR/(np.cos(self.sweep_angle_vertical)**2))**(0.357)*(self.taper_ratiov)**0.039
-        ###### Include Engine mass ######
+        self.iter += 1
 
-        ###### Include Landing Gear Group mass#####
+        ###### Engine mass ######
 
-        ###### Fuel System Estimation #######
+        ###### Landing Gear Group mass#####
+
+        ###### Fuel System mass #######
+
+    def classiter(self):
+        self.class1()
+        OEW1 = self.w_oew
+        self.class2()
+        OEW2 = self.w_oew
+        if np.abs(OEW2 - OEW1)/OEW2>=0.07:
+            self.classiter()
+        else:
+            pass
+
+
+    def sizing(self):
 
         pass
