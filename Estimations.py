69fd1c53156a45db9515adf153c94202e9ac9b2c
Tarek Abdelrazek <tarekuni@hotmail.com>
2022-05-17 12:00:15 +0200

    Added Estimation Files

---
+++ Estimations.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+from design_dictionaries import *
+
+
+
+class Aircraft:
+    def __init__(self,x_wing,x_cg,m_wing,m_fuselage,L_D_cruise,L_D_loiter,c_p,
+                 length_fus,height_fus,width_fus,diameter_fus,surface_wing
+                 ,w_design,t_c,lamda,w_mtow,w_oew,R,E,V,AR,
+                 sweep_angle,surface_controlv,surface_controlh,sweep_angle_horizontal,sweep_angle_vertical,
+                 m_v,m_h,taper_ratio,taper_ratioh,taper_ratiov,w_payload,w_crew,f_res,w_empty):
+        self.x_wing = x_wing
+        self.x_cg = x_cg
+        self.m_wing = m_wing
+        self.m_h = m_h
+        self.m_v = m_v
+        self.m_wing = m_wing
+        self.m_fuselage= m_fuselage
+        self.L_D_cruise = L_D_cruise
+        self.L_D_loiter = L_D_loiter
+        self.c_p = c_p
+        self.length_fus = length_fus
+        self.height_fus = height_fus
+        self.width_fus = width_fus
+        self.diameter_fus = diameter_fus
+        self.surface_wing = surface_wing
+        self.surface_controlv = surface_controlv
+        self.surface_controlh = surface_controlh
+        self.w_design = w_design
+        self.t_c = t_c
+        self.lamda = lamda
+        self.w_mtow = w_mtow
+        self.w_oew = w_oew
+        self.R = R
+        self.E = E
+        self.V = V
+        self.efficiency = 0.85
+        self.fractions = 0.992 * 0.996 * 0.996 * 0.990 * 0.992 * 0.992
+        self.AR=AR
+        self.sweep_angle = sweep_angle
+        self.sweep_angle_horizontal = sweep_angle_horizontal
+        self.sweep_angle_vertical = sweep_angle_vertical
+        self.q = 0.5*1.225*self.V**2
+        self.taper_ratio = taper_ratio
+        self.taper_ratioh = taper_ratioh
+        self.taper_ratiov = taper_ratiov
+        self.MTOWstat = np.multiply([14330, 16424, 46500, 22900, 25700, 12500, 15245, 11300, 12500, 8200, 9850, 14500, 36000, 8500, 45000, 34720, 5732, 7054, 28660, 44000, 41000, 21165, 26000, 9000],0.453592)
+        self.OEWstat = np.multiply([7716, 9072, 26560, 14175, 16075, 7750, 8500, 6494, 7538, 4915, 5682, 8387, 23693, 4613, 25525, 20580, 3245, 4299, 16094, 27000, 24635, 11945, 15510,  5018],0.453592)
+        self.a = linregress(self.MTOWstat, self.OEWstat).slope
+        self.b = linregress(self.MTOWstat, self.OEWstat).intercept
+        self.w_paylaod = w_payload
+        self.w_crew = w_crew
+        self.f_res= f_res
+        self.w_fuel = 0
+        self.w_empty = w_empty
+
+    def class1(self):
+        cruise_fraction = np.exp(self.R/self.efficiency/(9.81*self.c_p)*self.L_D_cruise)
+        f1 = 1/cruise_fraction
+        loiter_fraction = np.exp(self.E/self.efficiency/(self.V*9.81*self.c_p))*self.L_D_loiter
+        f2 = 1/loiter_fraction
+        fuel_coeff = 1-(f1*f2*self.fractions)
+        self.w_mtow = (self.w_paylaod+self.b+self.w_crew)/(1-self.a-fuel_coeff*(1-self.f_res))
+        self.w_oew = self.w_crew +self.w_empty
+        self.w_fuel = fuel_coeff* (1+self.f_res)*self.w_mtow
+        pass
+
+
+    def class2(self):
+        self.class1()
+        Ht_Hv = 1
+        self.m_fuselage = (12.7*(self.length_fus*self.diameter_fus)**(1.2982)*
+                          (1-(-0.008*(self.length_fus/self.diameter_fus)**(2)+
+                              0.1664*(self.length_fus/self.diameter_fus)-0.8501))*
+                          max(self.height_fus,self.width_fus)/self.diameter_fus)
+        self.m_wing = (0.0051*(self.w_design*1.5*2.5)**(0.557)*self.surface_wing**
+                      0.649*self.AR**0.5*(self.t_c)**(-0.4)*(1+self.lamda)**0.1*
+                      (np.cos(self.sweep_angle))**(-1)*(self.surface_controlv+self.surface_controlh)**0.1)
+        self.m_h = 0.016*(1.5*2.5*self.w_design)**0.414*self.q**(0.168)*self.surface_controlh**(0.896)*\
+                 (100*self.t_c/np.cos(self.sweep_angle_horizontal))**(-0.12)
+        self.m_v = 0.073*(1+0.2*(Ht_Hv))*(1.5*2.5*self.w_design)**0.376*self.q*0.122*self.surface_controlv**(0.873)*\
+                   (100*self.t_c/(np.cos(self.sweep_angle_vertical)))**(-0.49)*\
+                   (self.AR/(np.cos(self.sweep_angle_vertical)**2))**(0.357)*(self.taper_ratiov)**0.039
+        ###### Include Engine mass ######
+
+        ###### Include Landing Gear Group mass#####
+
+        ###### Fuel System Estimation #######
+
+        pass
--- Sensitivity.py
+++ Sensitivity.py
@@ -1,5 +1,6 @@
 import numpy as np
-import datetime
+
+
 data = dict()
 data['operations'] = {"Safety":np.array([4,6,4,3,4,3]),"Fuel storage":np.array([3,1,5,6,2,1]),"Refuelling":np.array([2,5,6,3,1,2]),"Maintenance":np.array([3,6,5,4,3,5]),"Weights":np.array([0.25,0.4,0.2,0.15]),"value":0.20}
 data['performance'] = {"Weight":np.array([7,1,5,6,4,1]),"Drag":np.array([7,1,4,6,5,1]),"Speed":np.array([7,1,7,7,6,1]),"Range":np.array([7,1,6,5,4,1]),"Weights":np.array([0.25,0.25,0.25,0.25]),"value":0.30}
@@ -16,7 +17,9 @@ def winner(data):
             if key!='Weights' and key!='value':
                 subwinner+=data[category][key]*data[category]['Weights'][i]
             i+=1
+        print(subwinner)
         winner+=subwinner*data[category]['value']
+    print(winner)
     f=np.where( winner == max(winner))[0][0]
     return f
 
@@ -27,6 +30,7 @@ def sensitivity(data):
     min = 1
     changedweights = np.array([[0.20,0.3,0.15,0.35],[0.25,0.25,0.25,0.25],[0.30,0.3,0.25,0.15],[0.20,0.2,0.2,0.4],[0.20,0.4,0.2,0.2]])
     weightsensetivity = np.zeros((len(changedweights),6))
+    categorysensetivity = np.zeros((len(changedweights),4))
     weightnumber = 0
     for weight in changedweights:
         changes = 0
@@ -37,23 +41,28 @@ def sensitivity(data):
         if winner(change) != original:
             print(f'design {winner(change)+1} wins if the weightage is {weight}')
         newdata = change
+        categorynumber = 0
         for category in change:
             for key in change[category]:
                 if key!='Weights' and key!='value':
                     for f in range(len(change[category][key])):
                             for i in range(-1,2,2):
-                                change[category][key][f]+=i*0.5
-                                changes+=1
                                 if winner(change)!=original and min <= change[category][key][f] <= max:
+                                    change[category][key][f] += i
+                                    changes += 1
                                     # print(f'in the {key} of {category} if you {i} the {f+1}th variable which was'
                                     #       f' \n originally {data[category][key][f]} the winner becomes design {winner(change)+1} for a weightage distribution of {weight}')
-                                    weightsensetivity[weightnumber][f]+=1
-                            change = newdata
+                                    weightsensetivity[weightnumber][winner(change)]+=1
+                                    categorysensetivity[weightnumber][categorynumber]+=1
+                                    change = newdata
+            categorynumber+=1
         weightsensetivity[weightnumber][4]= 204-sum(weightsensetivity[weightnumber])
         weightnumber+=1
+        change = newdata
     print(weightsensetivity)
+    print(categorysensetivity)
     pass
 
 
-
-sensitivity(data)
+winner(data)
+# sensitivity(data)
