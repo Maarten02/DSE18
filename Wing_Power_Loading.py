89bce75c87d3faa6851fffacf026bcdfafd55d16
theofalat <80906337+theofalat@users.noreply.github.com>
2022-05-18 16:03:32 +0200

    yes

--- Wing_Power_Loading.py
+++ Wing_Power_Loading.py
@@ -39,51 +39,7 @@ import numpy as np
 
   """
 
-
-
-"""
- Requirements that we need:
-
- Landing requirements
- --------------------
- --> Landing weight
- --> Approach speed VA
- --> Deceleration method used || Prolly brakes
- --> Flying qualities of the aircraft || High wing
- --> C_L_Max (including HLDs)
- 
- Cruise performance
- ----------------------
- --> propeller efficiency
- --> cruise speed
- --> cruise altitude
- --> drag polar
- --> weight fraction of MTOW at cruise
- --> power fraction of P_TO at cruise
-
- Climb rate performance
- ----------------------
- --> climb requirement
-
- Climb gradient performance
- ------------------------
- --> safety margin on lift coefficient
-
-
- Minimum speed   -----> 61kts
- Take off distance  ------> <1500m
- maneuvering: turn rate & radius  -----> Relavant Later
- maximum speed  ------>
- maximum altitude ----->   for now 3048m
-
-"""
 ISA_density = 1.225 #[kg/m^3]
-
-
-
-
-
-
 class PowerLoading:
     def __init__(self, MTOW):
         self.stall_speed = 41.7 #[m/s]
